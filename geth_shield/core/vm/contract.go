23e7795f2f203d72fff2e369427938e51cf20f54
helloswx <helloswx@126.com>
2023-10-29 14:59:28 +0800

    Update contract.go

--- geth_shield/core/vm/contract.go
+++ geth_shield/core/vm/contract.go
@@ -21,7 +21,6 @@ import (
 	"io/ioutil"
 
 	"math/big"
-	
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/holiman/uint256"
@@ -32,7 +31,6 @@ import (
 	"strings"
 	//"strconv"
 	//"reflect"
-
 	//mapset "github.com/deckarep/golang-set"
 	//"github.com/ethereum/go-ethereum/crypto"
 )
@@ -55,70 +53,74 @@ type AccountRef common.Address
 func (ar AccountRef) Address() common.Address { return (common.Address)(ar) }
 
 //【*】变量名对应的绑定信息
-const(
- Common = iota
- Package 
- Mapping
- Dynamic
+const (
+	Common = iota
+	Package
+	Mapping
+	Dynamic
 )
+
 type Variable struct {
-	Slot      []uint256.Int //所有slot
-	StartSlot uint256.Int    
-	PackageStart  int        //用于紧凑存储的情况,其他类型即为0
-	DataType int             //Common,Package,Dynamic,Mapping
-	DataSize   int           //对于静态类型是数据长度，对于动态数组是元素长度，单位字节
-	
-	DynamicHash uint256.Int  //
-	DynamicEnd uint256.Int   //
-	
+	Slot         []uint256.Int //所有slot
+	StartSlot    uint256.Int
+	PackageStart int //用于紧凑存储的情况,其他类型即为0
+	DataType     int //Common,Package,Dynamic,Mapping
+	DataSize     int //对于静态类型是数据长度，对于动态数组是元素长度，单位字节
+
+	DynamicHash uint256.Int //
+	DynamicEnd  uint256.Int //
+
 	SubValueType string //value的类型
-	SubVars []Variable
+	SubVars      []Variable
 }
 
-func (v *Variable) InitSlot(){
-	if(v.DataSize<=32){
-		if(v.DataSize==0){
+func (v *Variable) InitSlot() {
+	if v.DataSize <= 32 {
+		if v.DataSize == 0 {
 			v.DataSize = 32
 		}
-		v.Slot = make([]uint256.Int,1)
+		v.Slot = make([]uint256.Int, 1)
 		v.Slot[0] = v.StartSlot
-	
-	}else{
-		v.Slot = make([]uint256.Int,(v.DataSize-1)/32+1)	
+
+	} else {
+		v.Slot = make([]uint256.Int, (v.DataSize-1)/32+1)
 		s := v.StartSlot
-		for i:=0;i<(v.DataSize-1)/32+1;i++{
+		for i := 0; i < (v.DataSize-1)/32+1; i++ {
 			v.Slot[i] = s
 			s.Add(&s, uint256.NewInt(1))
 		}
 	}
-	if(v.DataType == Mapping || v.DataType == Dynamic){
-		v.SubVars = make([]Variable,0)
+	if v.DataType == Mapping || v.DataType == Dynamic {
+		v.SubVars = make([]Variable, 0)
 	}
 }
 
-func (v *Variable)Contains(who uint256.Int) bool{
-	for i:=0;i<len(v.Slot);i++{
-		if(v.Slot[i]==who){
+func (v *Variable) Contains(who uint256.Int) bool {
+	for i := 0; i < len(v.Slot); i++ {
+		if v.Slot[i] == who {
 			return true
 		}
 	}
-	if(v.DataType==Dynamic){
-	        res1 := v.DynamicHash.Cmp(&who)
+	if v.DataType == Dynamic {
+		res1 := v.DynamicHash.Cmp(&who)
 		res2 := v.DynamicEnd.Cmp(&who)
-		if(res1<=0&&res2==1){
+		if res1 <= 0 && res2 == 1 {
 			return true
 		}
 	}
 	return false
 }
-type Firewall struct{
+
+type Firewall struct {
 	Hash int
 	Vars []int
 }
-type Rule struct{
+type Rule struct {
 	Firewalls []Firewall
 	FocusVars []Variable
+	// CanSendEther bool
 }
+
 // Contract represents an ethereum contract in the state database. It contains
 // the contract code, calling arguments. Contract implements ContractRef
 type Contract struct {
@@ -141,31 +143,34 @@ type Contract struct {
 	value *big.Int
 
 	//【*】屏蔽规则
-	rule Rule
+	rule        Rule
 	CheckedHash int
-	TmpVars []Variable
+	TmpVars     []Variable
 }
-func getRuleByAddr(adr ContractRef) Rule{
+
+func getRuleByAddr(adr ContractRef) Rule {
 	var rule Rule
 	str, err := ioutil.ReadFile("rules/" + adr.Address().String())
-    	if(err!=nil){
-	        rule.FocusVars = make([]Variable,0);
-        	rule.Firewalls = make([]Firewall,0)
-	}else{
+	if err != nil {
+		rule.FocusVars = make([]Variable, 0)
+		rule.Firewalls = make([]Firewall, 0)
+
+	} else {
 		_ = json.Unmarshal([]byte(str), &rule) //第二个参数用来存放第一个参数的内容,c2需要被修改，想要被需改成功必须得传入指针
 	}
 	return rule
 }
-func Write(rule *Rule,adr ContractRef) {
+func Write(rule *Rule, adr ContractRef) {
 	data, err := json.MarshalIndent(rule, "", "	")
 	if err != nil {
 		panic(err)
 	}
-	err = ioutil.WriteFile("rules/" + adr.Address().String(), data, 0777)
+	err = ioutil.WriteFile("rules/"+adr.Address().String(), data, 0777)
 	if err != nil {
 		panic(err)
 	}
 }
+
 // NewContract returns a new contract environment for the execution of EVM.
 func NewContract(caller ContractRef, object ContractRef, value *big.Int, gas uint64) *Contract {
 	c := &Contract{CallerAddress: caller.Address(), caller: caller, self: object}
@@ -182,30 +187,29 @@ func NewContract(caller ContractRef, object ContractRef, value *big.Int, gas uin
 	c.Gas = gas
 	// ensures a value is set
 	c.value = value
-	
+
 	//【*】
 	c.rule = getRuleByAddr(object)
-	c.TmpVars = make([]Variable,0)
+	c.TmpVars = make([]Variable, 0)
 
 	return c
 }
-func add(slots *[]uint256.Int,slot uint256.Int){
-	for _,s := range *slots{
-		if(bytes.Equal(slot.Bytes(), s.Bytes())){
+func add(slots *[]uint256.Int, slot uint256.Int) {
+	for _, s := range *slots {
+		if bytes.Equal(slot.Bytes(), s.Bytes()) {
 			return
 		}
 	}
-	*slots = append(*slots,slot)
+	*slots = append(*slots, slot)
 }
 
-
 //【*】屏蔽逻辑,SSTORE时调用
-func (v *Variable) Shield(loc uint256.Int, val uint256.Int,interpreter *EVMInterpreter, scope *ScopeContext) bool {
+func (v *Variable) Shield(loc uint256.Int, val uint256.Int, interpreter *EVMInterpreter, scope *ScopeContext) bool {
 	if v.Contains(loc) {
-		if(v.DataType == Package){
+		if v.DataType == Package {
 			valueothers := val.Bytes32()
 			valueoriginal := interpreter.evm.StateDB.GetState(scope.Contract.Address(), common.BytesToHash(loc.Bytes()))
-			return bytes.Equal(valueothers[v.PackageStart:v.PackageStart+v.DataSize],valueoriginal[v.PackageStart:v.PackageStart+v.DataSize])
+			return bytes.Equal(valueothers[v.PackageStart:v.PackageStart+v.DataSize], valueoriginal[v.PackageStart:v.PackageStart+v.DataSize])
 		}
 		return false
 	}
@@ -216,106 +220,104 @@ func (v *Variable) Shield(loc uint256.Int, val uint256.Int,interpreter *EVMInter
 // 本函数的功能在于
 //给定slot，寻找是否为标记的变量
 //记录hash(slot)
-func (v *Variable) SSI(slot uint256.Int, hash uint256.Int, interpreter *EVMInterpreter, scope *ScopeContext, isShield bool){
-	if(v.DataType != Mapping && v.DataType != Dynamic){
+func (v *Variable) SSI(slot uint256.Int, hash uint256.Int, interpreter *EVMInterpreter, scope *ScopeContext, isShield bool) {
+	if v.DataType != Mapping && v.DataType != Dynamic {
 		return
 	}
-	if(bytes.Equal(slot.Bytes(), v.StartSlot.Bytes())){
-		clan := strings.Split(v.SubValueType,"->")
-		if(v.DataType == Mapping){		
-			if(len(clan)==1 && isShield){
-				for _,Evar := range scope.Contract.TmpVars{
-					if(Evar.StartSlot==hash){
+	if bytes.Equal(slot.Bytes(), v.StartSlot.Bytes()) {
+		clan := strings.Split(v.SubValueType, "->")
+		if v.DataType == Mapping {
+			if len(clan) == 1 && isShield {
+				for _, Evar := range scope.Contract.TmpVars {
+					if Evar.StartSlot == hash {
 						return
 					}
 				}
-				scope.Contract.TmpVars = append(scope.Contract.TmpVars,Variable{
-					StartSlot:hash,
-					DataType:Common,
+				scope.Contract.TmpVars = append(scope.Contract.TmpVars, Variable{
+					StartSlot: hash,
+					DataType:  Common,
 				})
 				scope.Contract.TmpVars[len(scope.Contract.TmpVars)-1].InitSlot()
 				//fmt.Println("TmpVars",scope.Contract.TmpVars)
-				
-			} else if(len(clan) != 1){
-				for _,Evar := range v.SubVars{
-					if(Evar.StartSlot==hash){
+
+			} else if len(clan) != 1 {
+				for _, Evar := range v.SubVars {
+					if Evar.StartSlot == hash {
 						return
 					}
 				}
 				Value := clan[0]
 				var Value_Number int
-				if(Value == "Mapping"){
+				if Value == "Mapping" {
 					Value_Number = Mapping
-				}else if(Value == "Dynamic"){
+				} else if Value == "Dynamic" {
 					Value_Number = Dynamic
 				}
-				newVar :=Variable{
-					StartSlot: hash,
+				newVar := Variable{
+					StartSlot:    hash,
 					PackageStart: 0,
-					DataType: Value_Number,
-					DataSize: v.DataSize,
+					DataType:     Value_Number,
+					DataSize:     v.DataSize,
 					SubValueType: clan[1],
 				}
 				newVar.InitSlot()
-				//if(Value_Number == Dynamic){	
+				//if(Value_Number == Dynamic){
 				//	newVar.SSI(slot, hash, interpreter, scope, isShield)
 				//}
-				v.SubVars = append(v.SubVars,newVar)
+				v.SubVars = append(v.SubVars, newVar)
 			}
-		}else{
+		} else {
 			var Len uint256.Int
 			Len.SetBytes(interpreter.evm.StateDB.GetState(scope.Contract.Address(), common.BytesToHash(slot.Bytes())).Bytes())
 			Zero := uint256.NewInt(0).Bytes32()
 			LenB := Len.Bytes32()
-			if(bytes.Equal(LenB[0:32],Zero[0:32])){
+			if bytes.Equal(LenB[0:32], Zero[0:32]) {
 				return
 			}
-			if(v.DataSize>=32){
-				per := uint64((v.DataSize-1)/32+1)
-				Len.Mul(&Len,uint256.NewInt(per))
-				
-			}else{
-				per := uint64(32/v.DataSize)
-				Len.Sub(&Len,uint256.NewInt(1))
-				Len.Div(&Len,uint256.NewInt(per))
-				Len.Add(&Len,uint256.NewInt(1))
+			if v.DataSize >= 32 {
+				per := uint64((v.DataSize-1)/32 + 1)
+				Len.Mul(&Len, uint256.NewInt(per))
+
+			} else {
+				per := uint64(32 / v.DataSize)
+				Len.Sub(&Len, uint256.NewInt(1))
+				Len.Div(&Len, uint256.NewInt(per))
+				Len.Add(&Len, uint256.NewInt(1))
 			}
-			if(v.DynamicHash.Cmp(&hash)!=0){
-				v.SubVars = make([]Variable,0)
+			if v.DynamicHash.Cmp(&hash) != 0 {
+				v.SubVars = make([]Variable, 0)
 				v.DynamicHash.SetBytes(hash.Bytes())
 			}
 			v.DynamicEnd.SetBytes(hash.Bytes())
-			v.DynamicEnd.Add(&v.DynamicEnd,&Len)	
-			if(len(clan)>1){
+			v.DynamicEnd.Add(&v.DynamicEnd, &Len)
+			if len(clan) > 1 {
 				Value := clan[0]
 				var Value_Number int
-				if(Value == "Mapping"){
+				if Value == "Mapping" {
 					Value_Number = Mapping
-				}else if(Value == "Dynamic"){
+				} else if Value == "Dynamic" {
 					Value_Number = Dynamic
 				}
 				Count := v.DynamicEnd
-				for(Count.Cmp(&v.DynamicEnd)!=0){
-					v.SubVars = append(v.SubVars,Variable{
-						StartSlot: Count,
+				for Count.Cmp(&v.DynamicEnd) != 0 {
+					v.SubVars = append(v.SubVars, Variable{
+						StartSlot:    Count,
 						PackageStart: 0,
-						DataType: Value_Number,
-						DataSize: v.DataSize,
-						SubValueType: clan[1],	
-					})	
-					Count.Sub(&Count,uint256.NewInt(1))
-				}		
+						DataType:     Value_Number,
+						DataSize:     v.DataSize,
+						SubValueType: clan[1],
+					})
+					Count.Sub(&Count, uint256.NewInt(1))
+				}
 			}
 		}
-	
-	}else if(len(v.SubVars)!=0){
-		for _,Evar :=range v.SubVars{
-			Evar.SSI(slot,hash,interpreter,scope,isShield)
+
+	} else if len(v.SubVars) != 0 {
+		for _, Evar := range v.SubVars {
+			Evar.SSI(slot, hash, interpreter, scope, isShield)
 		}
 	}
 }
-
-
 
 func (c *Contract) validJumpdest(dest *uint256.Int) bool {
 	udest, overflow := dest.Uint64WithOverflow()
